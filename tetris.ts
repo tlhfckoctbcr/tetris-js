e0e28a32d8f116865510db9f3dba4aff8aefd063
grantsru <grantsru@gmail.com>
2020-03-25 17:22:23 -0400

    Create TetrisCanvas class

--- tetris.ts
+++ tetris.ts
@@ -1,6 +1,22 @@
-const canvas = document.createElement("canvas");
-canvas.width = 240;
-canvas.height = 400;
+class TetrisCanvas {
+  width: number;
+  height: number;
+
+  constructor(width = 240, height = 400) {
+    this.width = width;
+    this.height = height;
+  }
+
+  createElement(): HTMLCanvasElement {
+    const canvas = document.createElement("canvas");
+    canvas.width = this.width;
+    canvas.height = this.height;
+    return canvas;
+  }
+}
 
 const tetrisEl = document.getElementById("tetris");
-tetrisEl.append(canvas);
+const tetrisCanvas = new TetrisCanvas().createElement();
+
+tetrisEl.append(tetrisCanvas);
+
