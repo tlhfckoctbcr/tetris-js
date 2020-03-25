a4db0089258f1d2c29a422a258eda6a6033fab7d
grantsru <grantsru@gmail.com>
2020-03-25 18:23:47 -0400

    Generate Tetri

--- tetris.ts
+++ tetris.ts
@@ -1,22 +1,58 @@
-class TetrisCanvas {
+class TetrisBoard {
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
   width: number;
   height: number;
 
   constructor(width = 240, height = 400) {
     this.width = width;
     this.height = height;
+
+    this.createElement();
+    this.createContext();
   }
 
-  createElement(): HTMLCanvasElement {
+  createContext(): void {
+    const context = this.canvas.getContext("2d");
+
+    context.scale(20, 20);
+    context.fillStyle = "#333";
+    context.fillRect(0, 0, this.width, this.height);
+    this.context = context;
+  }
+
+  createElement(): void {
     const canvas = document.createElement("canvas");
+
     canvas.width = this.width;
     canvas.height = this.height;
-    return canvas;
+    this.canvas = canvas;
+  }
+}
+
+class Tetri {
+  matrix = [
+    [0,0,0],
+    [1,1,1],
+    [0,1,0]
+  ];
+
+  constructor(context: CanvasRenderingContext2D) {
+    this.matrix.forEach((row, y) => {
+      row.forEach((value, x) => {
+        console.log(value);
+        if (!!value) {
+          context.fillStyle = "red";
+          context.fillRect(x, y, 1, 1);
+        }
+      });
+    });
   }
 }
 
 const tetrisEl = document.getElementById("tetris");
-const tetrisCanvas = new TetrisCanvas().createElement();
+const tetrisBoard = new TetrisBoard();
 
-tetrisEl.append(tetrisCanvas);
+tetrisEl.append(tetrisBoard.canvas);
 
+const tetrisPiece = new TetrisPiece(tetrisBoard.context);
